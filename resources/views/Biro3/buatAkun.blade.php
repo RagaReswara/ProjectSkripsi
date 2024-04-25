2359e640a164b0abb5bc57bce643398184034ae4
RagaReswara <ragareswara55@gmail.com>
2024-04-25 20:20:07 +0700

    tampilan buat akun

--- resources/views/Biro3/buatAkun.blade.php
+++ resources/views/Biro3/buatAkun.blade.php
@@ -16,70 +16,103 @@
             
             </div>
 
-            <h2 class="font-bold text-3xl mb-3">List Peminjaman Yang Sedang Berlangsung</h2>
+            <div class="flex flex-1 ">
+                <h2 class="font-bold text-3xl mb-3">DAFTAR AKUN</h2>
+
+                
+            </div>
             
-            <!-- TABEL HARIAN -->
+            <!-- TABEL AKUN FAKULTAS -->
+            <h2 class="mt-5 font-bold text-3xl mb-3 bg-gray-300 rounded-lg">Fakultas</h2>
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
-                                Instansi/Organisasi
+                                Email
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Tanggal
+                                Password
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Jam
+                                Nama Organisasi
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Nama Kegiatan
+                                Nomor Telepon
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Nama Penanggung Jawab
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Nomor Telepon/WA
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Lapangan
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Status
+                                Fakultas
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Himpunan Mahasiswa Sistem Informasi
+                                gor_fti@ukdw.ac.id
                             </th>
                             <td class="px-6 py-4">
-                                17 April 2024
+                                fti123
                             </td>
                             <td class="px-6 py-4">
-                                16.00 - 20.00
+                                Badan Eksekutif Mahasiswa Fakultas Teknologi Informasi (BEMFTI)
                             </td>
                             <td class="px-6 py-4">
-                                Basket
+                                077362128364
                             </td>
                             <td class="px-6 py-2">
-                                Vito Hermawan
-                            </td>
-                            <td class="px-6 py-4">
-                                086632711234
-                            </td>
-                            <td class="px-6 py-4">
-                                Full Lapangan
-                            </td>
-                            <td class="px-6 py-4">
-                                berlangsung
+                                Teknologi Informasi
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <!-- TABEL HARIAN -->
+            <!-- TABEL FAKULTAS -->
+
+            <!-- TABEL PRODI -->
+            <h2 class="mt-5 font-bold text-3xl mb-3 bg-gray-300 rounded-lg">PROGRAM STUDI</h2>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Email
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Password
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Nama Organisasi
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Nomor Telepon
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Program Studi
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                gor_si@ukdw.ac.id
+                            </th>
+                            <td class="px-6 py-4">
+                                si1234
+                            </td>
+                            <td class="px-6 py-4">
+                                Himpunan Mahasiswa Sistem Informasi (HMSI)
+                            </td>
+                            <td class="px-6 py-4">
+                                073884612389
+                            </td>
+                            <td class="px-6 py-2">
+                                Sistem Informasi
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- TABEL PRODI -->
 
             </div>
         </div>
